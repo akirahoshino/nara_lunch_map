70755372883ba786bf48f2bfb42de971f3656a9f
akirahoshino <awsosphic@gmail.com>
2020-03-28 17:42:23 +0000

    link shop&category models, change form type selectable categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    has_many :shops
+    # ラーメンというカテゴリには複数のお店が存在できる
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,4 @@
 class Shop < ApplicationRecord
+    belongs_to :category
+    # お店がどれか１つのカテゴリに所属する
 end
